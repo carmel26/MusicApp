b3de2225b5606f653d53488af9aeb9bdeb48eb55
carmel26 <nkeshipelagie@gmail.com>
2023-10-23 18:51:16 +0300

    removing some comment on 23Oct2023 18h51

--- musicApp/settings.py
+++ musicApp/settings.py
@@ -84,9 +84,7 @@ DATABASES = {
 }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
-
+ 
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -120,9 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
+ 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
  
 AUTH_USER_MODEL = 'rating_App.CustomUserTable'
